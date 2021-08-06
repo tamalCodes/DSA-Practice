1c277552c353e1b98e23a0c463d6ebe8a05c4f63
Tamal Das <gyansujan69@gmail.com>
2021-08-07 00:33:33 +0530

    updated kadanes-algo.cpp

--- Arrays/kadanes-algo.cpp
+++ Arrays/kadanes-algo.cpp
@@ -2,6 +2,36 @@
 using namespace std;
 int main()
 {
+ int n;
+ cin>>n;
+ int arr[n];
+ for (int i = 0; i < n; i++)
+ {
+    cin>>arr[i];
+ }
+
+ if(n==1)
+ {
+     cout<<arr[0];
+     return 0;
+ }
+ else
+ {
+     int sum=0,max=arr[0];
+     for (int i = 0; i < n; i++)
+     {
+        sum=sum+arr[i];
+        if(sum>max)
+        {
+            max=sum;
+        }
+
+        if(sum<0)
+        sum=0;
+     }
+     cout<<max;
+ }
+ 
  
  return 0;
 }
