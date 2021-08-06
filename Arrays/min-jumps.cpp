f955fa67d6a0a20859646b84db22483431ba5f0a
Tamal Das <gyansujan69@gmail.com>
2021-08-06 12:29:05 +0530

    updated min-jumps.cpp

--- Arrays/min-jumps.cpp
+++ Arrays/min-jumps.cpp
@@ -3,42 +3,42 @@ using namespace std;
 
 int minjumps(int arr[], int n)
 {
-    int i=0,count=0;
-    while (i<n)
+    if (arr[0] == 0)
+        return -1;
+    int jumps = 0, m = 0, halt = 0;
+
+    for (int i = 0; i < n - 1; i++)
     {
-       if(arr[i]==0)
-       {
-           return -1;
-       }
-       else
-       {
-           if(i==(n-1))
-           {
-               return count;
-           }
-           
-           i=i+arr[i];
-           count=count+1;
-       }
-       
+        m = max(m, i + arr[i + 1]);
+        if (m >= n - 1)
+        {
+            jumps = jumps + 1;
+            return jumps;
+        }
+
+        if (i == halt)
+        {
+            halt = m;
+            jumps = jumps + 1;
+        }
     }
-    
-    
-    return count;
+
+    if (halt >= n - 1)
+        return jumps;
+    else
+        return -1;
 }
-
-
 
 int main()
 {
- int n;
- cin>>n;
- int arr[n];
- for (int i = 0; i < n; i++)
- {
-    cin>>arr[i];
- }
- cout<<minjumps(arr,n);
- 
- return 0;
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << minjumps(arr, n);
+
+    return 0;
 }
