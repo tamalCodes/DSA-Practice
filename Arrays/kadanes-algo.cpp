1b6937a87eb3d6c4f57c8fab0a1c475fd3a536b9
Tamal Das <gyansujan69@gmail.com>
2021-08-07 09:15:21 +0530

    Added Comments

--- Arrays/kadanes-algo.cpp
+++ Arrays/kadanes-algo.cpp
@@ -1,3 +1,10 @@
+//The question is to find the max sum of a contingous subarray.
+//The array can even have negative integers so we dont care if the sum is in negative, we will simply make it 0
+//The logic is to iterate from 0 to n-1 and add the sum and store it in max, if the sum is greater than max then we update the value of max
+//And finally return max
+// arr[] = {1,2,3,-2,5}  Output:9
+
+
 #include <bits/stdc++.h>
 using namespace std;
 int main()
