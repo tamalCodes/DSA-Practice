1de8d31646409b7daa1da5ee6b939951d6920d32
Tamal Das <gyansujan69@gmail.com>
2021-08-07 12:32:46 +0530

    changes made ro Max_product_subarray.cpp

--- Arrays/Max_Product_subarray.cpp
+++ Arrays/Max_Product_subarray.cpp
@@ -1,3 +1,6 @@
+//This is a program to find out the maximum product of a subarray
+//The logic is to select an element and to give it choices wether it wants to multiply with the prev max or min
+
 #include <bits/stdc++.h>
 using namespace std;
 int main()
@@ -5,12 +8,12 @@ int main()
     int n;
     cin >> n;
     int arr[n];
-    int pro = 1, maxi = 0;
     for (int i = 0; i < n; i++) //taking input
     {
         cin >> arr[i];
     }
-
+    int choice1,choice2;
+        int maxi=arr[0],mini=arr[0],ans=arr[0]; 
     if (n == 1)
     {
         cout << "The product is: " << arr[0];
@@ -18,16 +21,19 @@ int main()
     else
     {
         
-        for (int i = 0; i < n; i++)
+        
+        
+        for (int i = 1; i < n; i++)
         {
-            pro = pro * arr[i];
-            if (pro <= 0)
-                pro = 1;
+            choice1 = arr[i]*maxi;
+            choice2 = arr[i]*mini;
 
-            if (pro > maxi)
-                maxi = pro;
+            maxi = max(arr[i],max(choice1,choice2));
+            mini = min(arr[i],min(choice1,choice2));
+            ans = max(ans,maxi);
         }
+        
     }
-    cout<<maxi;
+    cout<<ans;
     return 0;
 }
