3b80ae2bc2e57c5ba5cac578b91173e8638dd032
Tamal Das <gyansujan69@gmail.com>
2021-08-11 00:11:18 +0530

    updated Learningstrings.cpp

--- Strings/Learningstrings.cpp
+++ Strings/Learningstrings.cpp
@@ -27,5 +27,24 @@ int main()
     string c = "Understandable";
     c.erase(2, 3); //start index and number of characters to be deleted.
     cout << c;     //der wil be deleted
+
+    //in addition to all these we have size, find, length,
+    string demo = "Hello everyone i am Tamal"
+
+    s.length();
+    s.size();
+    s.find("am");
+    s.insert(2,"lol"); 
+    s.substr(0,3) //start from 0 and extract 3 characters.
+    
+    string s1 = "99";
+    int x=stoi(s1);  //converts string to int
+    string s2 = to_string(x);
+
+    
+    
+    //to sort a string:
+    int sortable = "edcba";
+    sort(sortable.begin(), sortable.end());   //This will sort everything from begin to end and give abcde
     return 0;
 }
