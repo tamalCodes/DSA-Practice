ffc50d52985b05f3515762373c6fabb4739f338b
Tamal Das <gyansujan69@gmail.com>
2021-08-12 20:39:16 +0530

    Made some changes to Tortoise_Hare.cpp

--- Arrays/Tortoise_Hare.cpp
+++ Arrays/Tortoise_Hare.cpp
@@ -2,15 +2,36 @@
 using namespace std;
 int main()
 {
- int n;
- cin>>n;
- int arr[n];
- for (int i = 0; i < n; i++)
- {
-     cin>>arr[i];
- }
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
 
- 
- 
- return 0;
+    int tortoise = arr[0];
+    int hare = arr[0];
+
+    //phase 1 where the tortoise and the hare competes
+    //hare moves by 2 steps while tortoise moves by 1
+    do
+    {
+        tortoise = arr[tortoise];
+        hare = arr[arr[hare]];
+    } while (tortoise != hare);
+
+    tortoise = arr[0];
+    
+    //phase 2 another chance given
+    //both of them moves at the same speed
+    while (tortoise != hare)
+    {
+        tortoise = arr[tortoise];
+        hare = arr[hare];
+    }
+
+    cout<<hare;
+
+    return 0;
 }
