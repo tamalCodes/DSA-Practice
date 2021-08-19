bfc4da9ed0d9a19996e902b8cf354de921b58440
Tamal Das <gyansujan69@gmail.com>
2021-08-19 19:46:02 +0530

    Created 3sums.cpp

---
+++ Arrays/3-Sums.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int threesum(int arr[],int n)
+{
+
+}
+
+
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    threesum(arr,n);
+}
