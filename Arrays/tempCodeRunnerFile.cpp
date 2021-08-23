1af764062acbc20ed0a8f04b627cc89c5875122e
Tamal Das <gyansujan69@gmail.com>
2021-08-24 01:34:46 +0530

    Created Common_elements.cpp

--- Arrays/tempCodeRunnerFile.cpp
+++ Arrays/tempCodeRunnerFile.cpp
@@ -1,9 +1,1 @@
-for (int i = 0; i < n; i++)
-        {
-            pro = pro * arr[i];
-            if (pro == 0)
-                pro = 1;
-
-            if (pro > maxi)
-                maxi = pro;
-        }
+cout<<"I is : "<<i<<endl;
