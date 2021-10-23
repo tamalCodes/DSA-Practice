3fb5d62d645234f15413cae4f183d8d570aa884d
Tamal Das <gyansujan69@gmail.com>
2021-10-23 20:38:42 +0530

    Single_LL

--- C Codes/LinkedLists/Single_LL/InsertAtPos.c
+++ C Codes/LinkedLists/Single_LL/InsertAtPos.c
@@ -16,17 +16,78 @@ node *create()
 }
 
 //inserting the value
-void insert()
+node *insert(node *head, int data, int pos)
 {
     node *ptr = create();
-    int pos;       //taking the position from the user
+    ptr->data = data;
+    // int pos;       //taking the position from the user
     int count = 1; //used for checks
 
     // we need the prev pointer for storing the previous memory adress whenever we are inserting something new
     node *t = head, *prev;
+
+    // our lists starts from 1 so if the input pos is 0 then it is invalid
+    // Also if the head is null then the input is invalid
+    if (pos <= 0 || (pos > count && head == NULL))
+    {
+        printf("Invalid");
+    }
+    else if (pos == 1)
+    {
+        ptr->next = head; // we attach the ptr to the front of the head
+        head = ptr;
+    }
+    else
+    {
+        // with the help of this while loop we come to the place we need
+        while (t != NULL && count < pos)
+        {
+            prev = t;
+            t = t->next;
+            count++;
+        }
+
+        if (pos > count)
+            printf("Invalid");
+        else
+        {
+            prev->next = ptr; //ataching the new node
+            ptr->next = t;
+        }
+    }
+    return head;
+}
+
+void traversal(node *head)
+{
+    while (head != NULL)
+    {
+        printf("%d ", head->data);
+        head = head->next;
+    }
 }
 
 int main()
 {
-    return 0;
+    node *head = create();
+    node *second = create();
+    node *third = create();
+    node *fourth = create();
+
+    head->data = 10;
+    head->next = second;
+
+    second->data = 20;
+    second->next = third;
+
+    third->data = 30;
+    third->next = fourth;
+
+    fourth->data = 40;
+    fourth->next = NULL;
+
+    // here we are updating the head
+    head = insert(head, 69, 3);
+    traversal(head);
+    // return 0;
 }
