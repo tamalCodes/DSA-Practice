563e293b841708a060f8af0dd38e599caa44d523
Tamal Das <gyansujan69@gmail.com>
2021-11-26 10:03:15 +0530

    Created BST

---
+++ BST/BST.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// allocating the memory
+typedef struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+
+} node;
+
+node *create()
+{
+    node *temp;                          //creating a node pointer
+    temp = (node *)malloc(sizeof(node)); // allocating memory in the node
+    printf("Enter data: ");              //setting data for left right values and returning it
+    scanf("%d", &temp->data);
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
+
+int main()
+{
+    node *Root = create();
+    node *a = create();
+    node *b = create();
+    node *c = create();
+    node *d = create();
+    node *e = create();
+    node *f = create();
+
+    Root->left = a;
+    Root->right = b;
+
+    a->left = c;
+    a->right = d;
+    b->left = e;
+    b->right = f;
+
+    postOrder(Root);
+}
