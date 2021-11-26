605429ac0c9f7a1bff349e21a3f218cd4f4104b2
Tamal Das <gyansujan69@gmail.com>
2021-11-27 00:57:43 +0530

    BST Insertion

--- BST/BST.c
+++ BST/BST.c
@@ -10,34 +10,48 @@ typedef struct node
 
 } node;
 
-node *create()
+node *create(int data)
 {
     node *temp;                          //creating a node pointer
     temp = (node *)malloc(sizeof(node)); // allocating memory in the node
-    printf("Enter data: ");              //setting data for left right values and returning it
-    scanf("%d", &temp->data);
+    temp->data = data;
     temp->left = NULL;
     temp->right = NULL;
     return temp;
 }
 
+node *insert(node *root, int data)
+{
+    if (root == NULL)
+        return create(data);
+    else if (data < root->data)
+        root->left = insert(root->left, data);
+    else if (data > root->data)
+        root->right = insert(root->right, data);
+
+    return root;
+}
+
+void inOrder(node *h)
+{
+    if (h != NULL)
+    {
+        inOrder(h->left);
+        printf("%d ", h->data);
+        inOrder(h->right);
+    }
+}
+
 int main()
 {
-    node *Root = create();
-    node *a = create();
-    node *b = create();
-    node *c = create();
-    node *d = create();
-    node *e = create();
-    node *f = create();
+    node *root = NULL;
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 70);
+    insert(root, 60);
+    insert(root, 80);
 
-    Root->left = a;
-    Root->right = b;
-
-    a->left = c;
-    a->right = d;
-    b->left = e;
-    b->right = f;
-
-    postOrder(Root);
+    inOrder(root);
 }
