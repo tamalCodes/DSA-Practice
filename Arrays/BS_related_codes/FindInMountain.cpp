dedae0b5c9a92addf405afef3efcde426810041f
Tamal Das <gyansujan69@gmail.com>
2021-12-31 22:05:32 +0530

    Final commit of 2021.

--- Arrays/BS_related_codes/FindInMountain.cpp
+++ Arrays/BS_related_codes/FindInMountain.cpp
@@ -1,5 +1,7 @@
 //^ LEETCODE HARD
 //* https://leetcode.com/problems/find-in-mountain-array/
+//* So the array that is given here is a mountain array that means the array will be increasing, having a peak value and after that decreasing.
+//* We are given with a target therefore we need to check for 3 probabilites. The target can be peak, lie on the left side of the peak. Lie to the right side of peak.
 
 #include <bits/stdc++.h>
 using namespace std;
