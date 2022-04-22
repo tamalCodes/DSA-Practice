78dc5edaafaa171597ef4cffff188775c39aa068
Tamal Das <gyansujan69@gmail.com>
2022-04-22 13:49:05 +0530

    Merge sort

--- Sem-4/Mergesort.cpp
+++ Sem-4/Mergesort.cpp
@@ -1,91 +1,96 @@
-// c++ program for implementation of MergeSort
+// Merge sort in C++
+
 #include <iostream>
-#include <stdlib.h>
 using namespace std;
 
-// Merges two subarrays of arr[].
-// First subarray is arr[l..m]
-// Second subarray is arr[m+1..r]
-void merge(int arr[], int l, int m, int r)
+// Merge two subarrays L and M into arr
+void merge(int arr[], int p, int q, int r)
 {
-    int i = 0, j = m + 1, k = 1;
 
-    //* LOGIC 1 : check for each element if the element is greater or lesser and then insert it in the sorted array and increment the index of the sorted array (k++) as well as i , j
+    // Create L ← A[p..q] and M ← A[q+1..r]
+    int n1 = q - p + 1;
+    int n2 = r - q;
 
-    while (i <= m && j <= r)
+    int L[n1], M[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[p + i];
+    for (int j = 0; j < n2; j++)
+        M[j] = arr[q + 1 + j];
+
+    // Maintain current index of sub-arrays and main array
+    int i, j, k;
+    i = 0;
+    j = 0;
+    k = p;
+
+    // Until we reach either end of either L or M, pick larger among
+    // elements L and M and place them in the correct position at A[p..r]
+    while (i < n1 && j < n2)
     {
-        if (arr[i] < arr[j])
+        if (L[i] <= M[j])
         {
-            // arr[i] is smaller than arr[j], so insert arr[i] at index k and increment k
-            arr[k] = arr[i];
+            arr[k] = L[i];
             i++;
-            k++;
         }
         else
         {
-            // arr[i] is greater than arr[j], so insert arr[j] at index k and increment k
-            arr[k] = arr[j];
+            arr[k] = M[j];
             j++;
-            k++;
         }
+        k++;
     }
 
-    //* LOGIC 2 : Insert the remaining elements of the first subarray / second subarray if any into the sorted array
-
-    // out of these 2 while loops only one of them will be executed since we know the value of i and j keeps increasing .
-    // for i, i is always <= m and j is always <= r
-
-    while (i <= m)
+    // When we run out of elements in either L or M,
+    // pick up the remaining elements and put in A[p..r]
+    while (i < n1)
     {
-        arr[k] = arr[i];
+        arr[k] = L[i];
         i++;
         k++;
     }
 
-    while (j <= r)
+    while (j < n2)
     {
-        arr[k] = arr[j];
+        arr[k] = M[j];
         j++;
         k++;
     }
-
-    // after these 2 while loops we can conclude that the elements of the first subarray / second subarray are inserted into the sorted array
 }
 
-//* mergesort function
+// Divide the array into two subarrays, sort them and merge them
 void mergeSort(int arr[], int l, int r)
 {
     if (l < r)
     {
-        //* Same as (l+r)/2, but avoids overflow for
-        //* large l and h
+        // m is the point where the array is divided into two subarrays
         int m = l + (r - l) / 2;
-
-        //* Sort first and second halves
-        //* first half is from 0 to m
-        //* tarporer half m to r
 
         mergeSort(arr, l, m);
         mergeSort(arr, m + 1, r);
+
+        // Merge the sorted subarrays
         merge(arr, l, m, r);
     }
 }
 
-// main function to do merge sort
+// Print the array
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+// Driver program
 int main()
 {
-    int arr[] = {12, 11, 13, 5, 6, 7};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int arr[] = {6, 5, 12, 10, 9, 1};
+    int size = sizeof(arr) / sizeof(arr[0]);
 
-    cout << "Given array is \n";
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
+    mergeSort(arr, 0, size - 1);
 
-    mergeSort(arr, 0, n - 1);
-
-    cout << "\n\nSorted array is \n";
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
-
+    cout << "Sorted array: \n";
+    printArray(arr, size);
     return 0;
 }
