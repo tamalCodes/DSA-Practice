0bb687ce8705ca5864f958f6fa3fe812a02431e9
Tamal Das <gyansujan69@gmail.com>
2022-08-30 14:16:35 +0530

    Time: 12 ms (85.73%), Space: 18.4 MB (74.91%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -2,28 +2,23 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         
-        int i=0,count=0,t=101;
+        if(nums.size() == 0)
+            return 0;
+        
+        int i=0;
+        
+        for(int j=1;j<nums.size();j++)
+        {
+            if(nums[j]!=nums[i])
+            {
+                 i++;
+                swap(nums[i],nums[j]);
+            }
+        }
     
-    for(int i=0;i<nums.size();i++)
-    {
-        
-        
-       if(nums[i]!=t)
-       {
-           t = nums[i]; //1
-           count+=1; //2
-       }else
-       {
-           nums[i]=101;
-       }
-        
-      
-            
-    }
-        sort(nums.begin(), nums.end());
-        return count;
-        
+        return i+1;
     }
 };
 
-// brute force 1 : since this is a sorted array what we can do is, we will keep checking for similar elements 
+// BF1 : Use hashset
+
