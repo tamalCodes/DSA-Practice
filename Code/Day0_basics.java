17cb19e8308d9d9c7ab1237a4faeea1d8e1f4604
Tamal <gyansujan69@gmail.com>
2023-01-03 23:51:56 +0530

    feat : Java day1

---
+++ Code/Day0_basics.java
@@ -0,0 +1,26 @@
+package Code;
+
+public class Day0_basics {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the world of Java");
+
+        // * Primitive data types */
+
+        int roll = 99;
+        boolean isLeader = true;
+        char letter = 'q';
+        double num = 200.22;
+        long largeIntegr = 2000000000000000000L;
+        float floatingint = 44444444444444444444444f;
+
+        // String is not primitive
+
+        // * Non primitive data types */
+
+        // String is a class
+        String name = "Rishabh";
+
+    }
+}
